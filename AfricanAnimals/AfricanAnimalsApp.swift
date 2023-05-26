8f4a51298cf9efd4aaa23ee279f55ab731708235
Prabjot Singh <prabjot.singh@andersencorp.com>
2023-05-26 06:22:14 -0400

    Initial project setup and added required assets in the project

---
+++ AfricanAnimals/AfricanAnimalsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfricanAnimalsApp.swift
+//  AfricanAnimals
+//
+//  Created by M_2195552 on 2023-05-26.
+//
+
+import SwiftUI
+
+@main
+struct AfricanAnimalsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
